55a20cc80a05671382ee42ea4691719a076cf573
Philip Paeps <Philip.Paeps@cl.cam.ac.uk>
2011-07-22 20:01:39 +0100

    cheri: hook basic configuration bits up to the u-boot build system
    Creates a new "cheri" architecture with a "mips" cpu underneath.  Currently,
    all the mips bits are symlinks to the generic mips stuff in u-boot or based
    on generic mips boards already there.  We will diverse where needed.

---
+++ include/configs/cheri-mips.h
@@ -0,0 +1,11 @@
+/*
+ * This file contains the configuration parameters for cheri-mips target.
+ *    Very basic for the time being.  Needs to expand!
+ */
+
+#ifndef __CONFIG_H
+#define __CONFIG_H
+
+#define CONFIG_MIPS64		1
+
+#endif /* __CONFIG_H */

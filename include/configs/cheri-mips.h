3b84b3f671f7c0c64700da1577508ad950c18e71
Philip Paeps <Philip.Paeps@cl.cam.ac.uk>
2011-07-25 17:31:27 +0100

    cheri: configure caches so u-boot can use them

--- include/configs/cheri-mips.h
+++ include/configs/cheri-mips.h
@@ -8,4 +8,10 @@
 
 #define CONFIG_MIPS64		1
 
+/* Cache layout. */
+#define CONFIG_SYS_ICACHE_SIZE		4096	/* 4 KByte total	*/
+#define CONFIG_SYS_ICACHELINE_SIZE	8	/* 8 bytes/line		*/
+#define CONFIG_SYS_DCACHE_SIZE		4096	/* 4 KByte total	*/
+#define CONFIG_SYS_DCACHELINE_SIZE	8	/* 8 bytes/line		*/
+
 #endif /* __CONFIG_H */
